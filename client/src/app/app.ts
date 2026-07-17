863c69f47c97b8ab112f95b46c94cbb3dd870ea6
Prantika Paul <paul.prantika94@gmail.com>
2026-07-17 22:27:16 +0530

    End of Section 5

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,22 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   private http = inject(HttpClient);
+  private accountService = inject(AccountService);
   protected readonly title = signal('Dating App');
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
   ngOnInit(): void {
-    this.http.get('https://localhost:7297/api/members').subscribe({
+    this.http.get<User[]>('https://localhost:7297/api/members').subscribe({
       next: response => {
         this.members.set(response);
-        console.log(this.members());
+        //console.log(this.members());
       },
       error: error => {
         console.log(error);
@@ -25,6 +30,14 @@ export class App implements OnInit {
         console.log('Completed the hhtp request.');
       }
     });
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 
 }
---
+++ client/src/features/home/home.ts
@@ -0,0 +1,19 @@
+import { Component, inject, signal } from '@angular/core';
+import { Register } from "../account/register/register";
+import { User } from '../../types/user';
+import { AccountService } from '../../core/services/account-service';
+
+@Component({
+  selector: 'app-home',
+  imports: [Register],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  protected accountService = inject(AccountService);
+  protected registerMode= signal(false);
+
+  showRegister(value:boolean){
+    this.registerMode.set(value);
+  }
+}
