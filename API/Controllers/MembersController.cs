501f86e794e5570bace981bf884ad8caabdcd2bd
Prantika Paul <paul.prantika94@gmail.com>
2026-07-22 18:00:08 +0530

    End of Section 8

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,27 +1,36 @@
 ﻿using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class MembersController(AppDbContext context) : BaseApiController
+    [Authorize]
+    public class MembersController(IMemberRepository memberRepository) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetMembers()
+        public async Task<ActionResult<IEnumerable<Member>>> GetMembers()
         {
-            var members = await context.Users.ToListAsync();
+            var members = await memberRepository.GetMembersAsync();
             return Ok(members);
         }
 
-        [Authorize]
         [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetMemberById(string id)
+        public async Task<ActionResult<Member>> GetMemberById(string id)
         {
-            var member = await context.Users.FindAsync(id);
+            var member = await memberRepository.GetMemberByIdAsync(id);
             if(member == null) { return NotFound(); }
             return Ok(member);  
+        }
+
+        [HttpGet("{id}/photos")]
+        public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
+        {
+            var memberPhotos = memberRepository.GetPhotosForMemberAsync(id);
+            if (memberPhotos == null) { return NotFound(); }
+            return Ok(memberPhotos);
         }
     }
 }
